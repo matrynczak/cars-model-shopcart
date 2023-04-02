db178d11bea4a56d7a9f26d5734740b571478afa
Mateusz Rynczak <mateuszrynczak@Mateuszs-MacBook-Air.local>
2023-04-02 20:15:10 +0200

    workaround of router

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter } from 'react-router-dom';
 import { useState } from 'react';
 import Homepage from "./components/Homepage/Homepage";
 import Shop from "./components/ShopPage/Shop";
@@ -20,14 +19,14 @@ const RouteSwitch = () => {
   }
     
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shop quantity={cartItems.length}/>} />
         <Route path="/shop/:id" element={<OfferItemPage quantity={cartItems.length} handleCart={handleCart}/>} />
         <Route path="/cart" element={<CartPage quantity={cartItems.length} cartItems={cartItems} handleCart={handleCart} clearCart={clearCart}/>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
